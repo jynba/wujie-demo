2c2bbd563064b1511bdf56472e6bd76137ad9121
jy <1239051474@qq.com>
2023-09-27 13:28:26 +0800

    init:wujie-demo

---
+++ main/src/router/index.ts
@@ -0,0 +1,12 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import HelloWorld from '../components/HelloWorld.vue';
+const router = createRouter({
+	history: createWebHistory(),
+	routes: [
+		{
+			path: '/',
+			component: HelloWorld,
+		},
+	],
+});
+export default router;
